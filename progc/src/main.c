c4d6ad24de64ce0d6d70a9d8ec2f19b2decee53e
Alouhette <133207604+Alouhette@users.noreply.github.com>
2024-01-12 15:45:05 +0000

    Avl creation p1 + struct client

--- progc/src/main.c
+++ progc/src/main.c
@@ -2,39 +2,43 @@
 #include <stdlib.h>
 #define Size_Max 100
 
-void D1(FILE *fData, int line_number){
-  if(fData == NULL){
+typedef struct _client {
+  int route_number;
+  int distance;
+} Client;
+
+void D1(FILE *fData, int line_number) {
+  if (fData == NULL) {
     exit(4);
   }
-  char chain[Size_Max]; //peut etre un malloc
-  //while(fgets(chain,Size_Max,fData != EOF){    
-  //}  
+  char chain[Size_Max]; // peut etre un malloc
+  // while(fgets(chain,Size_Max,fData != EOF){
+  // }
 }
 
-
-int main(int argc, char *argv[]){
-  if(argc-1 == 2){
+int main(int argc, char *argv[]) {
+  if (argc - 1 == 2) {
     printf("\n---Entering %s process---\n", argv[2]);
-  }
-  else{
+  } else {
     printf("C ERROR: Wrong number of arguments");
     exit(1);
   }
 
-  FILE* fData = NULL;
-  //if((fopen()))
-  // FILE* fopen(const char* nomDuFichier, const char* modeOuverture); 
-  // Mode Ouverture : r lecture seul , w ecriture seul , a ajouter fin , a+ lire et ecrire a la fin (si fichier existe pas il sera ajouter) ,r+ lire écrire (fichier DOIT etre créer), w+ pareil que r+ mais écrase tout 
+  FILE *fData = NULL;
+  // if((fopen()))
+  //  FILE* fopen(const char* nomDuFichier, const char* modeOuverture);
+  //  Mode Ouverture : r lecture seul , w ecriture seul , a ajouter fin , a+
+  //  lire et ecrire a la fin (si fichier existe pas il sera ajouter) ,r+ lire
+  //  écrire (fichier DOIT etre créer), w+ pareil que r+ mais écrase tout
 
   // Temporary code, show how the arguments work --
-  printf("There is %d argument to %s:", argc-1, argv[0]);
-  for(int i = 1; i<argc; i++){
+  printf("There is %d argument to %s:", argc - 1, argv[0]);
+  for (int i = 1; i < argc; i++) {
     printf("\n%d: %s", i, argv[i]);
   }
   //--
 
-  
   printf("\n---End of %s process---\n\n", argv[2]);
-  
+
   return 0;
 }
