5056a38fc5aee2d7eaf3f162c2256d135f7af1e0
Alouhette <133207604+Alouhette@users.noreply.github.com>
2024-01-27 14:34:56 +0100

    Create process.c

---
+++ progc/src/process.c
@@ -0,0 +1,27 @@
+void D1(FILE *fData, int line_number) {
+  if (fData == NULL) {
+    exit(4);
+  }
+  
+  // Créer un arbre des personnes
+  // boucle while pour lire les données de tout le fichier
+  // recup la ligne
+  // si la personne X dans arbre -> ajout et ajouter id trajet
+  // si la personnes est dedans incrementer la variable de +1
+
+  // free arbre ?
+}
+
+void D2(FILE *fData, int line_number) {
+  if (fData == NULL) {
+    exit(4);
+  }
+  // Créer un arbre des personnes
+
+  // boucle while pour lire les données de tout le fichier
+  // recup la ligne
+  // si la personne X dans arbre -> ajout
+  // si la personnes est dedans incrementer la distance
+
+  // free arbre ?
+}
