60913de6e759f311115df82e3e6cb7f280f0f835
Esteban <esteablz@gmail.com>
2024-01-28 04:33:25 +0100

    Ajout de la fonction readLine et ce qui va avec

--- progc/src/process.c
+++ progc/src/process.c
@@ -1,3 +1,99 @@
+#include "process.h"
+
+DataLine* init_ReadLine(FILE* fFile){
+  if(fFile == NULL){
+    printf("%s: pFile isn't properly given", __func__);
+    exit(4);
+  }
+
+  // Set the cursor to the start af the file
+  rewind(fFile);
+
+  // Get rid of the first line
+  while(fgetc(fFile) != '\n');
+
+  DataLine* pNew = NULL;
+  if((pNew = malloc(sizeof(DataLine))) == NULL){
+    printf("%s: pNew allocation didn't work", __func__);
+    exit(5);
+  }
+
+  pNew->route_ID = 0;
+  pNew->step_ID = 0;
+  pNew->town_A[0] = '\0';
+  pNew->town_B[0] = '\0';
+  pNew->distance = 0.0;
+  pNew->name[0] = '\0';
+
+  return pNew;
+}
+
+int readLine(FILE* fFile, DataLine* pLine){
+  if(fFile == NULL){
+      printf("%s: fFile isn't properly given", __func__);
+      exit(4);
+  }
+  if(pLine == NULL){
+      printf("%s: pLine isn't properly given", __func__);
+      exit(4);
+  }
+  
+  char c = 0;       // caracter being read
+  char string[100]; // column being read
+  int i = 0;        // 'c' caracter position in 'string'
+  int column = 0;
+
+  while(c != '\n' && c != EOF){
+    c = getc(fFile);
+      
+    if(c == ';' || c == '\n' || c == EOF){
+    string[i] = '\0';
+    
+    switch (column) {
+    case ROUTE_ID:
+        pLine->route_ID = atol(string);
+        break;
+    case STEP_ID:
+        pLine->step_ID = atoi(string);
+        break;
+    case TOWN_A:
+        sprintf(pLine->town_A, "%s", string);
+        break;
+    case TOWN_B:
+        sprintf(pLine->town_B, "%s", string);
+        break;
+    case DISTANCE:
+        pLine->distance = atof(string);
+        break;
+    case NAME:
+        sprintf(pLine->name, "%s", string);
+        break;
+    }
+
+    if(c == EOF){
+      string[i] = c; // A little complicated way to know when we trully hit the EOF
+    }
+      
+    column++;
+    i=-1;
+    }
+    else{
+      string[i] = c;
+    }
+
+    i++;
+    if(i >= 100){
+      printf("%s: The number of caracter of the column %d execced 100", __func__, column++);
+      exit(6);
+    }
+  }
+
+  return (string[0] != EOF); // Part of a little complicated way to know when we trully hit the EOF
+}
+
+
+
+
 void D1(FILE *fData, int line_number) {
   if (fData == NULL) {
     exit(4);
--- progc/src/process.h
+++ progc/src/process.h
@@ -8,4 +8,25 @@
 #define DISTANCE 4
 #define NAME 5
 
+#include <stdlib.h>
+#include <stdio.h>
+
+
+typedef struct{
+    long route_ID;
+    int step_ID;
+    char town_A[50];
+    char town_B[50];
+    double distance;
+    char name[50];
+
+}DataLine;
+
+
+DataLine* init_ReadLine(FILE* fFile);
+int readLine(FILE* fFile, DataLine* pLine);
+// Return 0 if the end of the file is reached
+// and 1 if not
+
+
 #endif
