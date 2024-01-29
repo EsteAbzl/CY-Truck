9a76f8d55fefcd45248fe4da7935bf8bf09ebf06
Alouhette <133207604+Alouhette@users.noreply.github.com>
2024-01-29 23:24:34 +0100

    Update AVL_Int.h

--- progc/src/AVL/AVL_Int.h
+++ progc/src/AVL/AVL_Int.h
@@ -21,17 +21,17 @@ AvlInt *createAvlInt(int v);
 // Out : Pointer to modified Avl
 // Adds in a new node to a BST. If the BST is an Avl,
 // it will remain balanced.
-AvlInt *addAvlInt(AvlInt *pTree, int elem);
+//AvlInt *addAvlInt(AvlInt *pTree, int elem);
 // Private ! Do not use !
-AvlInt *_addAvlInt(AvlInt *p, int v, int *h);
+AvlInt *addAvlInt(AvlInt *p, int v, int *h);
 
 
 // In : Pointer to Avl, String
 // Out : Pointer to modified Avl
 // Deletes the specified element from the tree.
-AvlInt *delAvlInt(AvlInt *pTree, int elem);
+//AvlInt *delAvlInt(AvlInt *pTree, int elem);
 // Private ! Do not use !
-AvlInt *_delAvlInt(AvlInt *pTree, int elem, int *h);
+AvlInt *delAvlInt(AvlInt *pTree, int elem, int *h);
 AvlInt *delAvlLargestInt(AvlInt *pTree, int *elem);
 
 
