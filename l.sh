203266bca41eb43eac2377b558112025317bd1d7
fleefie <waally2005@tuta.io>
2024-01-31 00:43:03 +0100

    This took me five minutes. Literally just a copy paste of D2 with an extra sort after the head, different cut fields and changing the awk variables. Same script. Same thing.

---
+++ l.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+time cat data.csv | sed -e 1d | cut -f1,5 -d";" | awk -F';' '{dist[$1] += $2} END{for (id in dist) printf "%s %.6f\n", id, dist[id]}' | sort -nr -k2 | head -n10 | sort -nr -k1
+
+# Nothing new here. Check D2 if you want an explaination for the awk script.
+# It's 42 past midnight as I'm writing this. I don't want to copypaste.
+#
+# Just in case though... sources used : https://www.gnu.org/software/gawk/manual/html_node/Basic-Printf.html explains printf
+#                                       https://www.gnu.org/software/gawk/manual/html_node/Array-Intro.html explains arrays
+#                                       https://www.gnu.org/software/gawk/manual/html_node/For-Statement.html for loop
